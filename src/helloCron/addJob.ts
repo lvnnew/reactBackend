0d872a216f30c72025e3ecc0dd1e493ec1b66a74
владимир Лещенко <pirat_2-5@mail.ru>
2022-07-07 21:49:05 +0300

    cron

---
+++ src/helloCron/addJob.ts
@@ -0,0 +1,21 @@
+import {config} from 'dotenv';
+import {quickAddJob} from 'graphile-worker';
+
+// yarn ts-node-dev src/helloCron/addJob.ts
+
+config();
+
+// Or add a job to be executed:
+const app = async () => {
+  for (let i = 0; i < 10; i++) {
+    await quickAddJob(
+      // makeWorkerUtils options
+      {connectionString: process.env.DATABASE_URL},
+
+      // Task identifier
+      'showDate',
+    );
+  }
+};
+
+app();
